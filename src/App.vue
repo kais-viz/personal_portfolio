3d3c301f245f3b2ce0a53b06da62e983e62de8cc
kais-viz <kaiskawar@gmail.com>
2025-05-15 17:06:42 +0200

    added hero

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <script setup>
 import Nav from './components/Nav.vue'
+import Hero from './components/Hero.vue'
 import Experience from './components/Experience.vue'
 import Education from './components/Education.vue'
 import Skills from './components/Skills.vue'
@@ -8,10 +9,13 @@ import Contact from './components/Contact.vue'
 
 <template>
   <Nav />
-  <Experience />
-  <Education />
-  <Skills />
-  <Contact />
+  <div class="container max-w-5xl px-4 mx-auto">  
+    <Hero />
+    <Experience />
+    <Education />
+    <Skills />
+    <Contact />
+  </div>
 </template>
 
 
