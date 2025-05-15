cd18ce7cf7e8ce515fa52a001f36d7ef3a0fef09
kais-viz <kaiskawar@gmail.com>
2025-05-15 16:56:53 +0200

    Added relevant sections using tailwind

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,16 @@
 <script setup>
 import Nav from './components/Nav.vue'
+import Experience from './components/Experience.vue'
+import Education from './components/Education.vue'
+import Skills from './components/Skills.vue'
 import Contact from './components/Contact.vue'
 </script>
 
 <template>
   <Nav />
+  <Experience />
+  <Education />
+  <Skills />
   <Contact />
 </template>
 
