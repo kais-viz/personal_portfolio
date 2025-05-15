dcc97f38f86e6a78e9566d7e745a4f4f94f3b9fa
kais-viz <kaiskawar@gmail.com>
2025-05-15 17:14:53 +0200

    Fixed education and experience components

--- src/components/Education.vue
+++ src/components/Education.vue
@@ -1,130 +1,63 @@
 <template>
-  <div class="flow-root">
-    <ul role="list" class="-mb-8">
-      <li v-for="(activityItem, activityItemIdx) in activity" :key="activityItem.id">
-        <div class="relative pb-8">
-          <span v-if="activityItemIdx !== activity.length - 1" class="absolute left-5 top-5 -ml-px h-full w-0.5 bg-gray-200" aria-hidden="true" />
-          <div class="relative flex items-start space-x-3">
-            <template v-if="activityItem.type === 'comment'">
+  <section id="education" class="py-10">
+    <h2 class="text-2xl font-bold mb-6">Education</h2>
+    <div class="flow-root">
+      <ul role="list" class="-mb-8">
+        <li v-for="(education, educationIdx) in educations" :key="education.id">
+          <div class="relative pb-8">
+            <span v-if="educationIdx !== educations.length - 1" class="absolute left-5 top-5 -ml-px h-full w-0.5 bg-gray-200" aria-hidden="true" />
+            <div class="relative flex items-start space-x-3">
               <div class="relative">
-                <img class="flex items-center justify-center bg-gray-400 rounded-full size-10 ring-8 ring-white" :src="activityItem.imageUrl" alt="" />
-
-                <span class="absolute -bottom-0.5 -right-1 rounded-tl bg-white px-0.5 py-px">
-                  <ChatBubbleLeftEllipsisIcon class="text-gray-400 size-5" aria-hidden="true" />
-                </span>
+                <img class="flex items-center justify-center bg-gray-100 rounded-full size-10 ring-8 ring-white" :src="education.schoolLogo" alt="" />
               </div>
               <div class="flex-1 min-w-0">
                 <div>
                   <div class="text-sm">
-                    <a :href="activityItem.person.href" class="font-medium text-gray-900">{{ activityItem.person.name }}</a>
+                    <span class="font-medium text-gray-900">{{ education.degree }}</span> at 
+                    <span class="font-medium text-gray-900">{{ education.school }}</span>
                   </div>
-                  <p class="mt-0.5 text-sm text-gray-500">Commented {{ activityItem.date }}</p>
+                  <p class="mt-0.5 text-sm text-gray-500">{{ education.location }} | {{ education.period }}</p>
                 </div>
                 <div class="mt-2 text-sm text-gray-700">
-                  <p>{{ activityItem.comment }}</p>
+                  <p>{{ education.description }}</p>
                 </div>
               </div>
-            </template>
-            <template v-else-if="activityItem.type === 'assignment'">
-              <div>
-                <div class="relative px-1">
-                  <div class="flex items-center justify-center bg-gray-100 rounded-full size-8 ring-8 ring-white">
-                    <UserCircleIcon class="text-gray-500 size-5" aria-hidden="true" />
-                  </div>
-                </div>
-              </div>
-              <div class="min-w-0 flex-1 py-1.5">
-                <div class="text-sm text-gray-500">
-                  <a :href="activityItem.person.href" class="font-medium text-gray-900">{{ activityItem.person.name }}</a>
-                  {{ ' ' }}
-                  assigned
-                  {{ ' ' }}
-                  <a :href="activityItem.assigned.href" class="font-medium text-gray-900">{{ activityItem.assigned.name }}</a>
-                  {{ ' ' }}
-                  <span class="whitespace-nowrap">{{ activityItem.date }}</span>
-                </div>
-              </div>
-            </template>
-            <template v-else-if="activityItem.type === 'tags'">
-              <div>
-                <div class="relative px-1">
-                  <div class="flex items-center justify-center bg-gray-100 rounded-full size-8 ring-8 ring-white">
-                    <TagIcon class="text-gray-500 size-5" aria-hidden="true" />
-                  </div>
-                </div>
-              </div>
-              <div class="flex-1 min-w-0 py-0">
-                <div class="text-gray-500 text-sm/8">
-                  <span class="mr-0.5">
-                    <a :href="activityItem.person.href" class="font-medium text-gray-900">{{ activityItem.person.name }}</a>
-                    {{ ' ' }}
-                    added tags
-                  </span>
-                  {{ ' ' }}
-                  <span class="mr-0.5">
-                    <template v-for="tag in activityItem.tags" :key="tag.name">
-                      <a :href="tag.href" class="inline-flex items-center gap-x-1.5 rounded-full px-2 py-1 text-xs font-medium text-gray-900 ring-1 ring-inset ring-gray-200">
-                        <svg :class="[tag.color, 'size-1.5']" viewBox="0 0 6 6" aria-hidden="true">
-                          <circle cx="3" cy="3" r="3" />
-                        </svg>
-                        {{ tag.name }}
-                      </a>
-                      {{ ' ' }}
-                    </template>
-                  </span>
-                  <span class="whitespace-nowrap">{{ activityItem.date }}</span>
-                </div>
-              </div>
-            </template>
+            </div>
           </div>
-        </div>
-      </li>
-    </ul>
-  </div>
+        </li>
+      </ul>
+    </div>
+  </section>
 </template>
 
 <script setup>
-import { ChatBubbleLeftEllipsisIcon, TagIcon, UserCircleIcon } from '@heroicons/vue/20/solid'
-
-const activity = [
-
-
+const educations = [
   {
     id: 1,
-    type: 'comment',
-    person: { name: 'Eduardo Benz', href: '#' },
-    imageUrl:
-      'https://images.unsplash.com/photo-1520785643438-5bf77931f493?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=256&h=256&q=80',
-    comment:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tincidunt nunc ipsum tempor purus vitae id. Morbi in vestibulum nec varius. Et diam cursus quis sed purus nam. ',
-    date: '6d ago',
+    degree: 'Master of Computer Science',
+    school: 'Stanford University',
+    schoolLogo: 'https://images.unsplash.com/photo-1522071820081-009f0129c71c?ixlib=rb-1.2.1&auto=format&fit=facearea&facepad=8&w=256&h=256&q=80',
+    description: 'Specialized in Artificial Intelligence and Machine Learning. Completed thesis on neural network optimization techniques. Participated in research projects and published papers in international conferences.',
+    location: 'Stanford, CA',
+    period: '2018 - 2020',
   },
   {
     id: 2,
-    type: 'assignment',
-    person: { name: 'Hilary Mahy', href: '#' },
-    assigned: { name: 'Kristin Watson', href: '#' },
-    date: '2d ago',
+    degree: 'Bachelor of Science in Computer Engineering',
+    school: 'MIT',
+    schoolLogo: 'https://images.unsplash.com/photo-1507537297725-24a1c029d3ca?ixlib=rb-1.2.1&auto=format&fit=facearea&facepad=8&w=256&h=256&q=80',
+    description: 'Graduated with honors. Focused on software engineering and systems design. Completed capstone project on distributed systems. Active member of the Robotics Club.',
+    location: 'Cambridge, MA',
+    period: '2014 - 2018',
   },
   {
     id: 3,
-    type: 'tags',
-    person: { name: 'Hilary Mahy', href: '#' },
-    tags: [
-      { name: 'Bug', href: '#', color: 'fill-red-500' },
-      { name: 'Accessibility', href: '#', color: 'fill-indigo-500' },
-    ],
-    date: '6h ago',
-  },
-  {
-    id: 4,
-    type: 'comment',
-    person: { name: 'Jason Meyers', href: '#' },
-    imageUrl:
-      'https://images.unsplash.com/photo-1531427186611-ecfd6d936c79?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=256&h=256&q=80',
-    comment:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tincidunt nunc ipsum tempor purus vitae id. Morbi in vestibulum nec varius. Et diam cursus quis sed purus nam. Scelerisque amet elit non sit ut tincidunt condimentum. Nisl ultrices eu venenatis diam.',
-    date: '2h ago',
+    degree: 'High School Diploma',
+    school: 'Tech Preparatory Academy',
+    schoolLogo: 'https://images.unsplash.com/photo-1519389950473-47ba0277781c?ixlib=rb-1.2.1&auto=format&fit=facearea&facepad=8&w=256&h=256&q=80',
+    description: 'Graduated with highest honors. Participated in national programming competitions. Founded the school\'s Computer Science Club. Received scholarship for academic excellence.',
+    location: 'Seattle, WA',
+    period: '2010 - 2014',
   },
 ]
 </script>
--- src/components/Experience.vue
+++ src/components/Experience.vue
@@ -1,130 +1,63 @@
 <template>
-  <div class="flow-root">
-    <ul role="list" class="-mb-8">
-      <li v-for="(activityItem, activityItemIdx) in activity" :key="activityItem.id">
-        <div class="relative pb-8">
-          <span v-if="activityItemIdx !== activity.length - 1" class="absolute left-5 top-5 -ml-px h-full w-0.5 bg-gray-200" aria-hidden="true" />
-          <div class="relative flex items-start space-x-3">
-            <template v-if="activityItem.type === 'comment'">
+  <section id="experience" class="py-10">
+    <h2 class="text-2xl font-bold mb-6">Experience</h2>
+    <div class="flow-root">
+      <ul role="list" class="-mb-8">
+        <li v-for="(job, jobIdx) in jobs" :key="job.id">
+          <div class="relative pb-8">
+            <span v-if="jobIdx !== jobs.length - 1" class="absolute left-5 top-5 -ml-px h-full w-0.5 bg-gray-200" aria-hidden="true" />
+            <div class="relative flex items-start space-x-3">
               <div class="relative">
-                <img class="flex items-center justify-center bg-gray-400 rounded-full size-10 ring-8 ring-white" :src="activityItem.imageUrl" alt="" />
-
-                <span class="absolute -bottom-0.5 -right-1 rounded-tl bg-white px-0.5 py-px">
-                  <ChatBubbleLeftEllipsisIcon class="text-gray-400 size-5" aria-hidden="true" />
-                </span>
+                <img class="flex items-center justify-center bg-gray-100 rounded-full size-10 ring-8 ring-white" :src="job.companyLogo" alt="" />
               </div>
               <div class="flex-1 min-w-0">
                 <div>
                   <div class="text-sm">
-                    <a :href="activityItem.person.href" class="font-medium text-gray-900">{{ activityItem.person.name }}</a>
+                    <span class="font-medium text-gray-900">{{ job.position }}</span> at 
+                    <span class="font-medium text-gray-900">{{ job.company }}</span>
                   </div>
-                  <p class="mt-0.5 text-sm text-gray-500">Commented {{ activityItem.date }}</p>
+                  <p class="mt-0.5 text-sm text-gray-500">{{ job.location }} | {{ job.period }}</p>
                 </div>
                 <div class="mt-2 text-sm text-gray-700">
-                  <p>{{ activityItem.comment }}</p>
+                  <p>{{ job.description }}</p>
                 </div>
               </div>
-            </template>
-            <template v-else-if="activityItem.type === 'assignment'">
-              <div>
-                <div class="relative px-1">
-                  <div class="flex items-center justify-center bg-gray-100 rounded-full size-8 ring-8 ring-white">
-                    <UserCircleIcon class="text-gray-500 size-5" aria-hidden="true" />
-                  </div>
-                </div>
-              </div>
-              <div class="min-w-0 flex-1 py-1.5">
-                <div class="text-sm text-gray-500">
-                  <a :href="activityItem.person.href" class="font-medium text-gray-900">{{ activityItem.person.name }}</a>
-                  {{ ' ' }}
-                  assigned
-                  {{ ' ' }}
-                  <a :href="activityItem.assigned.href" class="font-medium text-gray-900">{{ activityItem.assigned.name }}</a>
-                  {{ ' ' }}
-                  <span class="whitespace-nowrap">{{ activityItem.date }}</span>
-                </div>
-              </div>
-            </template>
-            <template v-else-if="activityItem.type === 'tags'">
-              <div>
-                <div class="relative px-1">
-                  <div class="flex items-center justify-center bg-gray-100 rounded-full size-8 ring-8 ring-white">
-                    <TagIcon class="text-gray-500 size-5" aria-hidden="true" />
-                  </div>
-                </div>
-              </div>
-              <div class="flex-1 min-w-0 py-0">
-                <div class="text-gray-500 text-sm/8">
-                  <span class="mr-0.5">
-                    <a :href="activityItem.person.href" class="font-medium text-gray-900">{{ activityItem.person.name }}</a>
-                    {{ ' ' }}
-                    added tags
-                  </span>
-                  {{ ' ' }}
-                  <span class="mr-0.5">
-                    <template v-for="tag in activityItem.tags" :key="tag.name">
-                      <a :href="tag.href" class="inline-flex items-center gap-x-1.5 rounded-full px-2 py-1 text-xs font-medium text-gray-900 ring-1 ring-inset ring-gray-200">
-                        <svg :class="[tag.color, 'size-1.5']" viewBox="0 0 6 6" aria-hidden="true">
-                          <circle cx="3" cy="3" r="3" />
-                        </svg>
-                        {{ tag.name }}
-                      </a>
-                      {{ ' ' }}
-                    </template>
-                  </span>
-                  <span class="whitespace-nowrap">{{ activityItem.date }}</span>
-                </div>
-              </div>
-            </template>
+            </div>
           </div>
-        </div>
-      </li>
-    </ul>
-  </div>
+        </li>
+      </ul>
+    </div>
+  </section>
 </template>
 
 <script setup>
-import { ChatBubbleLeftEllipsisIcon, TagIcon, UserCircleIcon } from '@heroicons/vue/20/solid'
-
-const activity = [
-
-
+const jobs = [
   {
     id: 1,
-    type: 'comment',
-    person: { name: 'Eduardo Benz', href: '#' },
-    imageUrl:
-      'https://images.unsplash.com/photo-1520785643438-5bf77931f493?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=256&h=256&q=80',
-    comment:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tincidunt nunc ipsum tempor purus vitae id. Morbi in vestibulum nec varius. Et diam cursus quis sed purus nam. ',
-    date: '6d ago',
+    position: 'Senior Software Engineer',
+    company: 'Tech Solutions Inc.',
+    companyLogo: 'https://images.unsplash.com/photo-1520785643438-5bf77931f493?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=256&h=256&q=80',
+    description: 'Led development of a cloud-based SaaS platform. Implemented CI/CD pipelines and microservices architecture. Mentored junior developers and conducted code reviews.',
+    location: 'San Francisco, CA',
+    period: 'Jan 2020 - Present',
   },
   {
     id: 2,
-    type: 'assignment',
-    person: { name: 'Hilary Mahy', href: '#' },
-    assigned: { name: 'Kristin Watson', href: '#' },
-    date: '2d ago',
+    position: 'Full Stack Developer',
+    company: 'Digital Innovations',
+    companyLogo: 'https://images.unsplash.com/photo-1531427186611-ecfd6d936c79?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=256&h=256&q=80',
+    description: 'Developed responsive web applications using Vue.js and Node.js. Designed and implemented RESTful APIs. Collaborated with UX designers to improve user experience.',
+    location: 'Austin, TX',
+    period: 'Mar 2018 - Dec 2019',
   },
   {
     id: 3,
-    type: 'tags',
-    person: { name: 'Hilary Mahy', href: '#' },
-    tags: [
-      { name: 'Bug', href: '#', color: 'fill-red-500' },
-      { name: 'Accessibility', href: '#', color: 'fill-indigo-500' },
-    ],
-    date: '6h ago',
-  },
-  {
-    id: 4,
-    type: 'comment',
-    person: { name: 'Jason Meyers', href: '#' },
-    imageUrl:
-      'https://images.unsplash.com/photo-1531427186611-ecfd6d936c79?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=256&h=256&q=80',
-    comment:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tincidunt nunc ipsum tempor purus vitae id. Morbi in vestibulum nec varius. Et diam cursus quis sed purus nam. Scelerisque amet elit non sit ut tincidunt condimentum. Nisl ultrices eu venenatis diam.',
-    date: '2h ago',
+    position: 'Frontend Developer',
+    company: 'WebCraft Studios',
+    companyLogo: 'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&auto=format&fit=facearea&facepad=8&w=256&h=256&q=80',
+    description: 'Built interactive user interfaces using modern JavaScript frameworks. Optimized application performance and implemented responsive designs. Participated in agile development processes.',
+    location: 'Seattle, WA',
+    period: 'Jun 2016 - Feb 2018',
   },
 ]
 </script>
