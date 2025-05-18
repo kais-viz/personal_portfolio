83922e984e786b50c580cc9d3af6fff49f1619e5
kais-viz <kaiskawar@gmail.com>
2025-05-18 15:37:38 +0200

    updated cv, change hero text

--- src/components/HeroNew.vue
+++ src/components/HeroNew.vue
@@ -5,11 +5,10 @@
       <div class="px-6 py-12 mx-auto max-w-7xl sm:py-16 lg:px-8">
         <div class="flex flex-col items-start gap-8 md:flex-row">
           <!-- Left side content -->
-          <div class="w-full space-y-6 md:w-1/2">
-            <p class="text-lg text-gray-600 dark:text-gray-300">Hello, I'm</p>
-            
-            <h1 class="text-4xl font-bold text-gray-800 dark:text-white md:text-5xl -ml-0.5">Kais Kawar</h1>
-            <p class="text-lg text-gray-600 dark:text-gray-300">A passionate software developer specializing in building modern web applications with a focus on clean code and user experience.</p>
+          <div class="w-full space-y-6 md:w-1/2">          
+            <h1 class="text-4xl font-bold text-gray-800 dark:text-white md:text-4xl -ml-0.5">Delivering High-Impact Software Solutions.</h1>
+            <p class="text-2xl text-gray-600 dark:text-gray-300">I'm Kais Kawar, a Full Stack Software Engineer specializing in building robust, data-driven, and scalable cloud-based applications.</p>
+            <p class="text-lg text-gray-600 dark:text-gray-300">Leveraging Python, Node.js, Vue.js, and AWS, I deliver innovative solutions from reliable ETL pipelines to high-performance web applications, ensuring your data works for you and your systems handle growth effortlessly.</p>
           </div>
           <!-- Right side image -->
           <div class="flex justify-center w-full md:w-1/2">
